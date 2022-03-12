c1e90e6c2eaa83895c916b26df63dcac0bbadf45
LastOnSpitfire <101465273+LastOnSpitfire@users.noreply.github.com>
2022-03-12 16:43:10 +0100

    Add files via upload

--- config.cpp
+++ config.cpp
@@ -1,63 +1,194 @@
 class CfgPatches
 {
-	class LDA_cassette
+	class sound
 	{
-		units[]={};
+		units[]=
+		{
+		};
 		weapons[]={};
 		requiredVersion=0.1;
 		requiredAddons[]=
 		{
-			"LDA_cassette_story",
-			"DZ_Radio",
-			"DZ_Gear_Consumables",
-			"DZ_Data"
+			"DZ_Data",
+			"DZ_Sounds_Effects",
+			"LDA_cassette"
 		};
 	};
 };
-class CfgMods
+class CfgWorlds
 {
-    class LDA_cassette
-    {
-        dir = "LDA_cassette";
-        picture = "";
-        action = "";
-        hideName = 1;
-        hidePicture = 1;
-        name = "LDA_cassette";
-        credits = "";
-        author = "";
-        authorID = "0"; 
-        version = "1.0"; 
-        extra = 0;
-        type = "mod";
-
-        dependencies[] = {"Game", "World", "Mission"};
-        
-        class defs
-        {
-			class imageSets 
+	class CAWorld;
+	class Enoch: CAWorld
+	{
+		class EnvSounds
+		{
+			class CfgEnvSpatialSounds
 			{
-				files[]=
+				class sound_play_cassette1
 				{
-					"LDA_cassette/gui/imagesets/SF_Cassette_slot.imageset",
-					"LDA_cassette/gui/imagesets/SF_GhostImage_slot.imageset"
+					memPoint="sound_disfx_playground";
+					soundSets[]=
+					{
+						"Amazing_Grace_SoundSet",
+					};
 				};
 			};
-            class gameScriptModule
-            {
-                value = "";
-                files[] = {"LDA_cassette/Scripts/3_Game"};
-            };
-            class worldScriptModule
-            {
-                value = "";
-                files[] = {"LDA_cassette/Scripts/4_World"};
-            };
-            class missionScriptModule
-            {
-                value = "";
-                files[] = {"LDA_cassette/Scripts/5_Mission"};
-            };
-        };
+		};
 	};
 };
+class CfgSoundShaders
+{
+	class baseCharacter_SoundShader;
+	class basecassette_SoundShader: baseCharacter_SoundShader
+	{
+		range=10;
+	};
+	class play_Soundshader: basecassette_SoundShader
+	{
+		samples[]=
+		{
+			
+			{
+				"LDA_cassette\Model\sound\play1",
+				1
+			}
+		};
+		volume=1;
+	};
+	class stop_Soundshader: basecassette_SoundShader
+	{
+		samples[]=
+		{
+			
+			{
+				"LDA_cassette\Model\sound\stop1",
+				1
+			},
+			
+			{
+				"LDA_cassette\Model\sound\stop2",
+				1
+			},
+		};
+		volume=1;
+	};
+	class ouverture_Soundshader: basecassette_SoundShader
+	{
+		samples[]=
+		{
+			
+			{
+				"LDA_cassette\Model\sound\ouverture",
+				1
+			},
+			
+			{
+				"LDA_cassette\Model\sound\ouverture2",
+				1
+			}
+		};
+		volume=1;
+	};
+	class fermeture_Soundshader: basecassette_SoundShader
+	{
+		samples[]=
+		{
+			
+			{
+				"LDA_cassette\Model\sound\fermeture",
+				1
+			}
+		};
+		volume=1;
+	};
+	class rembobinage_Soundshader: basecassette_SoundShader
+	{
+		samples[]=
+		{
+			
+			{
+				"LDA_cassette\Model\sound\rembobinage",
+				1
+			}
+		};
+		volume=1.5;
+	};
+	class Amazing_Grace_Soundshader: basecassette_SoundShader
+	{
+		samples[]=
+		{
+			
+			{
+				"LDA_cassette\Model\sound\Amazing_Grace_United_Pipers_for_Peace",
+				1
+			},
+		};
+		volume=1;
+	};
+};
+class CfgSoundSets
+{
+	class baseCharacter_SoundSet;
+	class play_SoundSet: baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"play_Soundshader"
+		};
+	};
+	class stop_SoundSet: baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"stop_Soundshader"
+		};
+	};
+	class ouverture_SoundSet: baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"ouverture_Soundshader"
+		};
+	};
+	class fermeture_SoundSet: baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"fermeture_Soundshader"
+		};
+	};
+	class rembobinage_SoundSet: baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"rembobinage_Soundshader"
+		};
+	};
+	class Amazing_Grace_SoundSet: baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"Amazing_Grace_Soundshader"
+		};
+	};
+};
+/*
+class CfgSounds
+{
+	class default
+	{
+		name="";
+		titles[]={};
+	};
+	class doorWoodRattle_1: default
+	{
+		sound[]=
+		{
+			"\DZ\sounds\environment\buildings\doors\WoodRattle\doorWoodRattle_1",
+			0.56234133,
+			1,
+			30
+		};
+	};
+};
+*/
